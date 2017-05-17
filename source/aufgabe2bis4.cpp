76a307d5f6da1a2c87b29e7272282403d1cf0d01
Jonas Haffky <jonas.david.benno.haffky@uni-weimar.de>
2017-05-17 20:45:43 +0200

    aufgabe2bis4 done,  add aufgabe5

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -1,5 +1,9 @@
+# include <iostream> //std :: cout
+# include <set> //std :: set
+# include <cstdlib> //std :: rand ()
 # include <vector> //std :: vector < >
 # include <list> //std :: list < >
+# include <map> //std :: map < > 
 
 int main (){
     std::list<unsigned int> l0(100);
@@ -9,4 +13,29 @@ int main (){
     std :: vector<int> v0(100);
     std::copy (std::begin(l0) , std::end(l0),
     std::begin(v0));
+
+    std::set< unsigned int> set;
+    for (auto& l : l0){
+        set.insert(l);
+        std::cout<<l<<", ";
+    }
+
+    std::cout <<"\nEs sind "<<set.size()<<" unterschiedliche Zahlen in der Liste."<<std::endl;
+    
+    std::cout <<"\nDiese Zahlen sind nicht in der Liste:"<<std::endl;
+
+    for(unsigned int i =0;i<=set.size();i++){
+        if(set.find(i) == set.end()){std::cout<<i<<", ";}
+    }
+
+    std::map<unsigned int, unsigned int>myMap;
+    for (auto& m : l0){
+        myMap[m]++;
+    }
+
+    std::cout <<"\n\nDie Häufigkeit jeder Zahl ist:"<<std::endl;
+    for (auto& m : myMap){
+        std::cout<<m.first<<" : "<<m.second<<std::endl;
+    } 
+
 }
---
+++ source/aufgabe5.cpp
@@ -0,0 +1,15 @@
+# define CATCH_CONFIG_ R UN N E R
+# include <catch.hpp>
+# include <cmath>
+# include <algorithm>
+
+TEST_CASE ("filter alle vielfache von drei", "[ erase ]")
+{
+// ihre Loesung :
+// ...
+}
+REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3));
+int main(int argc, char* argv[])
+{
+return Catch::Session().run(argc, argv);
+}
